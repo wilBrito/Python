d6416c3867ae72bafea9f2be7177d64051bfb126
Wilson Brito <wil.brito@icloud.com>
2023-05-11 17:24:30 +0200

    Terminamos el primer día del curso de Python (print,string,input)

---
+++ Project_01/Primer_Proyecto.py
@@ -0,0 +1,18 @@
+
+
+print("1. Empezamos aprendiendo a imprimir textos y números")
+
+print("Hello World")
+print(555)
+
+print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+
+print("2. Strings")
+
+print("Hola " + "Mundo")
+
+print("+++++++++++++++++++++++++++++++++++++++++++++++++")
+
+print("2. Input")
+print(input("Tu Nombre:"))
+
---
+++ Project_01/Proyecto_Dia_01.py
@@ -0,0 +1,3 @@
+
+
+print("El nombre de la cerveza es\n" + "\"" + input("Di un animal:") + " " + input("Di un color:") + "\"")
