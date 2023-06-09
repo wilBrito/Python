4050f0705122d1ead42be86a27302ad57de1fd56
Wilson Brito <wil.brito@icloud.com>
2023-06-09 18:45:37 +0200

    Primer case del ejecicio de recetas terminado 1/6

--- Python_Curso/Proyectos Diarios/Proyecto_Día_06.py
+++ Python_Curso/Proyectos Diarios/Proyecto_Día_06.py
@@ -3,6 +3,8 @@ from pathlib import Path
 
 
 ruta_principal = Path('C:\\Users\wilso\Desktop\Cursos Udemy\Python\Python_Curso\Recetas')
+list_categorias = []
+list_receta = []
 
 def contar_recetas():
     initial_count = 0
@@ -29,17 +31,34 @@ def elegir_categoria():
     print("__________________________")
     for i, path in enumerate(ruta_principal.iterdir()):
         print(f"[{i+1}] {os.path.basename(path)}")
+        list_categorias.append(os.path.basename(path))
         cont += 1
-
     while opcion <= 0 or opcion > cont:
         opcion = int(input("Elige una categoria: "))
 
     return opcion
 
-#def leer_receta():
+def mostrar_recetas(categoria_seleccionada):
+    path = ruta_principal / list_categorias[categoria_seleccionada - 1]
+    opcion = 0
+    cont = 0
+    print("__________________________")
+    for i, p in enumerate(path.iterdir()):
+        print(f"[{i+1}] {os.path.basename((p))}")
+        list_receta.append(os.path.basename((p)))
+        cont += 1
 
+    while opcion <= 0 or opcion > cont:
+        opcion = int(input("Elige una receta para leer: "))
 
+    return opcion
 
+def leer_receta(categoria_seleccionada,receta_seleccionada):
+    print("__________________________")
+    path = ruta_principal / list_categorias[categoria_seleccionada - 1] / list_receta[receta_seleccionada - 1]
+    archivo = open(path, 'r')
+    print(archivo.read())
+    archivo.close()
 def programa_recetas():
     opc = 0
     while opc <= 0 or opc > 6:
@@ -48,6 +67,8 @@ def programa_recetas():
     match opc:
         case 1:
             categoria = elegir_categoria()
+            receta = mostrar_recetas(categoria)
+            leer_receta(categoria, receta)
         #case 2:
 
         #case 3:
