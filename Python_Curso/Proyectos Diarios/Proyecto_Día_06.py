6e0c9c7fbe1f157040d1e7a58c959e86ac8e3fca
Wilson Brito <wil.brito@icloud.com>
2023-06-16 17:41:06 +0200

    Terminado Proyecto del dia 6, Recetario

--- Python_Curso/Proyectos Diarios/Proyecto_Día_06.py
+++ Python_Curso/Proyectos Diarios/Proyecto_Día_06.py
@@ -2,9 +2,11 @@ import  pathlib ,os
 from pathlib import Path
 
 
+
 ruta_principal = Path('C:\\Users\wilso\Desktop\Cursos Udemy\Python\Python_Curso\Recetas')
 list_categorias = []
 list_receta = []
+finalizado = False
 
 def contar_recetas():
     initial_count = 0
@@ -38,6 +40,9 @@ def elegir_categoria():
 
     return opcion
 
+def eliminar_categoria(categoria):
+    os.rmdir(ruta_principal / list_categorias[categoria - 1])
+
 def mostrar_recetas(categoria_seleccionada):
     path = ruta_principal / list_categorias[categoria_seleccionada - 1]
     opcion = 0
@@ -60,12 +65,37 @@ def leer_receta(categoria_seleccionada,receta_seleccionada):
     print(archivo.read())
     archivo.close()
 
+def eliminar_receta(categoria,receta):
+    os.remove(ruta_principal / list_categorias[categoria - 1] / list_receta[receta - 1])
+
+def nombre_contenido_receta():
+    print("__________________________")
+    nombre = input("Escribe un nombre: ") + ".txt"
+    contenido = input("Escribe su contenido: ")
+    return nombre, contenido
+
+def crear_receta(categoria, nombre, contenido):
+    print("__________________________")
+    path = ruta_principal / list_categorias[categoria - 1] / nombre
+    archivo = open(path, 'a')
+    archivo.write(contenido)
+    archivo.close()
+
+
+def nombre_categoria():
+    print("__________________________")
+    categoria = input("Escribe un nombre: ")
+    return  categoria
+
+def crear_categoria(nombre):
+    os.mkdir(ruta_principal / nombre)
 
 
 
 def programa_recetas():
     opc = 0
     while opc <= 0 or opc > 6:
+        os.system('cls')
         opc = int(elegir_opciones())
 
     match opc:
@@ -73,16 +103,23 @@ def programa_recetas():
             categoria = elegir_categoria()
             receta = mostrar_recetas(categoria)
             leer_receta(categoria, receta)
-        #case 2:
-
-        #case 3:
-
-        #case 4:
-
-        #case 5:
-
-        #case 6:
-
+        case 2:
+            categoria = elegir_categoria()
+            nombre, contenido = nombre_contenido_receta()
+            crear_receta(categoria, nombre, contenido)
+        case 3:
+            nombre = nombre_categoria()
+            crear_categoria(nombre)
+        case 4:
+            categoria = elegir_categoria()
+            receta = mostrar_recetas(categoria)
+            eliminar_receta(categoria,receta)
+        case 5:
+            categoria = elegir_categoria()
+            eliminar_categoria(categoria)
+        case 6:
+            finalizado = True
+            return finalizado
 
 
 
@@ -91,4 +128,5 @@ Las recetas están en {ruta_principal}
 tienes {contar_recetas()} recetas''')
 
 
-programa_recetas()
+while not finalizado:
+    finalizado = programa_recetas()
