f7fab133b1156729468bebd7f563f53df4aeb712
Wilson Brito <wil.brito@icloud.com>
2023-07-01 11:33:09 +0200

    en proceso

---
+++ Python_Curso/Día 9/prueba.py
@@ -0,0 +1,14 @@
+from collections import Counter
+
+lista = [1, 2, 3, 6, 7, 1, 2, 4, 5, 5, 5, 5, 3, 2, 6, 7]
+
+contador = Counter(lista)
+
+
+
+
+from datetime import datetime
+
+
+minutos = datetime.now().minute
+print(minutos)
